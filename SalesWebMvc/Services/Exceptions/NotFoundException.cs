f22ffed4786e81e7b1cfa0ea59f1e04abc3f9f2f
Rillian Diello Lucas Pires <rillianpires@sescms.com.br>
2020-05-26 13:42:27 -0400

    Update seller

---
+++ SalesWebMvc/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace SalesWebMvc.Services.Exceptions{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message){
+            
+        }
+    }
+}
