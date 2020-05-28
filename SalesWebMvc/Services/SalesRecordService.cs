f22dcb9b0326db137d351a1d304c79cf2d86dc74
Rillian Diello <rilliansf@gmail.com>
2020-05-27 23:15:05 -0400

    Grouping search

--- SalesWebMvc/Services/SalesRecordService.cs
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -34,5 +34,32 @@ namespace SalesWebMvc.Services
             .OrderByDescending(x => x.Date)
             .ToListAsync();
         }
+
+        /// <summary>
+        /// Metodo que faz uma busca agrupada. Uma busca agrupada retorna um IGrouping pelo agrupamento com o os Dados
+        /// Ou seja estamos fazendo um agrupamento de Departments, retornando suas vendas
+        /// </summary>
+        /// <param name="minDate"></param>
+        /// <param name="maxDate"></param>
+        /// <returns></returns>
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGrouping(DateTime? minDate, DateTime? maxDate)
+        {
+
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return await result.Include(x => x.Seller)
+            .Include(x => x.Seller.Department)
+            .OrderByDescending(x => x.Date)
+            .GroupBy(x => x.Seller.Department)
+            .ToListAsync();
+        }
     }
 }
